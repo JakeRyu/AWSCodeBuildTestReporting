9255ea38d0677337c274175bdc5d61ef3f645729
Jake Ryu <jake.ryu@hotmail.co.uk>
2020-06-22 16:20:22 +0900

    Add buildspec.yml

---
+++ CodeBuildDotnetTestReportExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeBuildDotnetTestReportExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
